3cdd317f5016299cb129fffeb32d9c8e18d46e03
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2023-12-21 11:36:10 +0530

    successfully implemented function as props

--- src/components/Way1.tsx
+++ src/components/Way1.tsx
@@ -1,30 +1,9 @@
 import { Button } from 'react-bootstrap';
 
-type Color = 'red' | 'blue' | 'green' | 'white';
-
-type ButtonProps = {
-  backgroundColor: Color;
-  fontSize: number;
-  textColor: Color;
-  padding: [number, number, number, number];
+type FunctionProps = {
+  onClick: (a: string) => number[];
 };
 
-export const Way1 = ({
-  backgroundColor,
-  fontSize,
-  textColor,
-  padding,
-}: ButtonProps) => {
-  return (
-    <Button
-      style={{
-        backgroundColor: backgroundColor,
-        color: textColor,
-        fontSize: fontSize,
-        padding: `${padding[0]}px ${padding[1]}px ${padding[2]}px ${padding[3]}px`,
-      }}
-    >
-      {backgroundColor}
-    </Button>
-  );
+export const Way1 = ({ onClick }: FunctionProps) => {
+  return <Button onClick={() => onClick('hello')}>Click</Button>;
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import { Way2 } from '../components/Way2';
+import { Way1 } from '../components/Way1';
 
 export const Home = () => {
+  const onClick = (a: string): number[] => {
+    console.log([0]);
+    return [0];
+  };
   return (
     <>
       <h1>Home</h1>
-      {/* to also specify the key types the 
-      we use Record to do that */}
-      <Way2
-        borderRadius={{
-          topLeft: 5,
-          topRight: 5,
-          bottomRight: 5,
-          bottomLeft: 5,
-        }}
-      />
+      <Way1 onClick={onClick} />
     </>
   );
 };
