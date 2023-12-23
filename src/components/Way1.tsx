f124ed803cfac63cbf41ec2aa8e2d955f6fae1be
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2023-12-23 12:42:20 +0530

    my code

--- src/components/Way1.tsx
+++ src/components/Way1.tsx
@@ -1,28 +1,3 @@
-type Company = {
-  id: string;
-  companyName: string;
-  industry: string;
-  employeeRange: string;
-};
-
-type EmailAddress = {
-  id: string;
-  emailAddress: string;
-  emailType: string;
-};
-
-type Role = {
-  id: string;
-  roleCode: string;
-  roleName: string;
-};
-
-type StrengthFinder = {
-  id: string;
-  strengths: string[];
-  weakness: string[];
-};
-
 type Address = {
   id: string;
   country: string;
@@ -32,30 +7,32 @@ type Address = {
   state: string;
 };
 
-type AdditionalInfo = {
+type StrengthFinder = {
   id: string;
-  strengthFinder: StrengthFinder;
-  emergencyContacts: any[]; // You may want to define a type for this too
-  address: Address;
+  strengths: string[];
+  weakness: string[];
 };
 
-type User = {
+type Role = {
   id: string;
-  username: string;
-  firstName: string;
-  lastName: string;
-  bio: string;
-  active: boolean;
-  isDeleted: boolean;
-  profileUrl: string;
-  emailAddresses: EmailAddress[];
-  phoneNumbers: string[]; // Assuming phone numbers are strings
-  role: Role;
-  additionalInfo: AdditionalInfo;
-  userTeam: any[]; // You may want to define a type for this too
+  roleCode: string;
+  roleName: string;
 };
 
-type Accountability = {
+type EmailAddresses = {
+  id: string;
+  emailAddress: string;
+  emailType: string;
+};
+
+type Companies = {
+  id: string;
+  companyName: string;
+  industry: string;
+  employeeRange: string;
+};
+
+type AccountabilitiesList = {
   id: string;
   name: string;
   description: string;
@@ -64,7 +41,7 @@ type Accountability = {
   creationTimestamp: string;
 };
 
-type Attachment = {
+type AttachmentsList = {
   id: string;
   uploaded: boolean;
   fileName: string;
@@ -74,37 +51,61 @@ type Attachment = {
   creationTimestamp: string;
 };
 
-// Define the main types for the parent data
-type Seat = {
+type AdditionalInfo = {
+  id: string;
+  strengthFinder: StrengthFinder;
+  emergencyContacts: any[];
+  address: Address;
+};
+
+type Teams = {
+  name: string;
+  id: string;
+  creationTimestamp: string;
+  isLt: boolean;
+  isProject: boolean;
+  isPrivateTeam: boolean;
+  users: EosUserList[];
+  companies: Companies[];
+};
+
+type EosUserList = {
+  id: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  bio?: string;
+  active: boolean;
+  isDeleted: boolean;
+  isInvited?: boolean;
+  profileUrl?: string;
+  emailAddresses: EmailAddresses[];
+  phoneNumbers: string[];
+  role: Role;
+  additionalInfo?: AdditionalInfo;
+  emergencyContacts?: any[];
+  userTeam: any[];
+  teams?: Teams[];
+};
+
+type ChildSeats = {
   id: string;
   name: string;
   visionary: boolean;
-  company: Company;
+  company: Companies;
   parent_id?: string;
-  childSeats: Seat[];
-  eosUserList: User[];
-  accountabilitiesList: Accountability[];
-  attachmentsList: Attachment[];
+  childSeats: ChildSeats[];
+  eosUserList: EosUserList[];
+  accountabilitiesList: AccountabilitiesList[];
+  attachmentsList: AttachmentsList[];
 };
 
-// type ParentData = {
-//   id: string;
-//   name: string;
-//   visionary: boolean;
-//   company: Company;
-//   childSeats: Seat[];
-//   eosUserList: User[];
-//   accountabilitiesList: Accountability[];
-//   attachmentsList: Attachment[];
-// };
+type ParentData = ChildSeats;
 
-type ParentData = Seat;
-
-// Use the types as the prop type for the child component
 type ChildComponentProps = {
   data: ParentData[];
 };
 
 export const Way1 = ({ data }: ChildComponentProps) => {
-  return <div>data</div>;
+  return <div>{data[0].childSeats[0].id}</div>;
 };
