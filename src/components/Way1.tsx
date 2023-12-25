f819cf09b056d9d1100a2f85e371c5d0839a2f17
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2023-12-25 16:56:58 +0530

    use like this when you want to pass properties of a tag

--- src/components/Way1.tsx
+++ src/components/Way1.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { ComponentProps, ComponentPropsWithoutRef } from 'react';
 
-// type color = 'red' | 'blue' | 'yellow';
+// intstead of button we can also use anchor tag and image tag etc
+// type ButtonProps = ComponentProps<'button'>;
 
-interface Color {
-  color: 'red' | 'blue' | 'yellow';
-}
+// if ref is not forwared from parent component use this
+type ButtonProps = ComponentPropsWithoutRef<'button'>;
 
-export const Way1 = () => {
+export const Way1 = ({ type, autoFocus }: ButtonProps) => {
   return <div>Hello</div>;
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,11 @@ import { Way1 } from '../components/Way1';
 export const Home = () => {
   return (
     <>
-      {/* in interface you cannot use union just like you used type 
-      you can only do that if you describe it as a object*/}
-      <Way1 />
+      {/* if we have hundred of this attribute what you are going to do */}
+      {/* what you need to do is natively wrapping */}
+      {/* you basically want to allow all those attribute to passed as a prop */}
+      {/* and we dont want to specify all of them individually one by one */}
+      <Way1 type='submit' autoFocus={true} />
     </>
   );
 };
