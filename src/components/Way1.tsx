241dd1b0992aa0945b82095a43fe03de8efc451b
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2023-12-29 19:15:06 +0530

    example 2

--- src/components/Way1.tsx
+++ src/components/Way1.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 export const Way1 = () => {
-  const getIndexofArrayItem = <T,>(arr: T[], arrItem: T) => {
-    return arr.findIndex((item: T) => item === arrItem);
+  const getIndexofArrayItem = <T, K>(arr1: T, arr2: K): [T, K] => {
+    return [arr1, arr2];
   };
 
   const arr: number[] = [55, 97, 65];
-  const index = getIndexofArrayItem(arr, 65);
+  const str: string[] = ['hello', 'bye'];
+  const index = getIndexofArrayItem(arr, str);
 
   return (
     <div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Way1 } from '../components/Way1';
 export const Home = () => {
   return (
     <>
-      {/* generic example 1 */}
+      {/* generic example 2 */}
       <Way1 />
     </>
   );
