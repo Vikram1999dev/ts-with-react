0ddc10ff3e4b72dbd8564f682aae97675b61865e
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2024-01-06 23:28:22 +0530

    still improvmenet is required

--- src/components/Way1.tsx
+++ src/components/Way1.tsx
@@ -1,53 +1,61 @@
 import React from 'react';
-import CircularInput from './CircularInput';
 
 export const Way1 = () => {
   return (
-    <form>
-      <>
-        <h3>
-          <strong>Add New Course</strong>
-        </h3>
-        <div className='d-flex img-upload-container'>
-          {/* <input type='file' className='circle-img-input' accept='image/*' /> */}
-          <CircularInput />
-          {/* <img src={upload} alt='upload-img' className='upload-image' /> */}
-        </div>
-        <div className='d-flex flex-lg-row flex-md-row flex-sm-column'>
-          <div className='d-flex flex-column col-lg-6 col-md-6 col-sm-12'>
-            <label htmlFor='course-title'>Course Title</label>
-            <input
-              type='text'
-              name='course-title'
-              placeholder='Enter course/ event name'
-            />
-          </div>
-
-          <div className='d-flex flex-column col-lg-6 col-md-6 col-sm-12 file-upload-container'>
-            <label htmlFor='cover-img-upload'>Upload Cover Image</label>
-            <input type='file' name='cover-img-upload' id='file-input' />
-          </div>
-        </div>
-
+    <div
+      className='d-flex'
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+      }}
+    >
+      <div
+        className='col-lg-2 col-md-2'
+        style={{ backgroundColor: '#ddd' }}
+      ></div>
+      <div
+        className='d-flex flex-column'
+        style={{ width: '200px', position: 'fixed', backgroundColor: '#ddd' }}
+      >
+        <h2>List item 1</h2>
+        <h2>List item 2</h2>
+        <h2>List item 3</h2>
+        <h2>List item 4</h2>
+        <h2>List item 5</h2>
+        <h2>List item 6</h2>
+      </div>
+      <div className='col-lg-10 col-md-10' style={{ padding: '20px' }}>
         <div className='d-flex flex-column'>
-          <label htmlFor='course-desc'>Course Description</label>
-          <input
-            type='text'
-            name='course-desc'
-            placeholder='Enter course/ event description'
-          />
+          <h2>Main Content</h2>
+          <p>Main content goes here...</p>
+          <h2>Main Content</h2>
+          <p>Main content goes here...</p>
+          <h2>Main Content</h2>
+          <p>Main content goes here...</p>
+          <h2>Main Content</h2>
+          <p>Main content goes here...</p>
+          <h2>Main Content</h2>
+          <p>Main content goes here...</p>
+          <h2>Main Content</h2>
+          <p>Main content goes here...</p>
+          <h2>Main Content</h2>
+          <p>Main content goes here...</p>
+          <h2>Main Content</h2>
+          <p>Main content goes here...</p>
+          <h2>Main Content</h2>
+          <p>Main content goes here...</p>
+          <h2>Main Content</h2>
+          <p>Main content goes here...</p>
+          <h2>Main Content</h2>
+          <p>Main content goes here...</p>
+          <h2>Main Content</h2>
+          <p>Main content goes here...</p>
+          <h2>Main Content</h2>
+          <p>Main content goes here...</p>
         </div>
-
-        <div className='d-flex flex-column'>
-          <label htmlFor='will-learn'>What you will learn in course</label>
-          <textarea
-            name="'will-learn"
-            cols={30}
-            rows={10}
-            placeholder='Description'
-          ></textarea>
-        </div>
-      </>
-    </form>
+      </div>
+    </div>
   );
 };
