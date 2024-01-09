6099090fc207385eae89a8b0c3f571349563b88f
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2024-01-09 11:28:33 +0530

    great useful scenarios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,8 @@ import { Way1 } from '../components/Way1';
 export const Home = () => {
   return (
     <>
-      {/* here we are using unknown type
-    because for example we don't know what we will
-    get as data what will be its type and also to 
-    avoid 'any' type */}
+      {/* A list of TypeScript types you will likely use in a React+TypeScript app: */}
+      {/* probably some errors but very important to keep in mind */}
       <Way1 />
     </>
   );
