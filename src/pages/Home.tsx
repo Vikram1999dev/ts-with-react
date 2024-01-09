ec7fcc9ff36ecc2a5ad5752236b0bda1e4efe5f7
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2024-01-09 11:31:48 +0530

    component as props

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Way1 } from '../components/Way1';
 export const Home = () => {
   return (
     <>
-      {/* A list of TypeScript types you will likely use in a React+TypeScript app: */}
+      {/* Relevant for components that accept other React components as props. */}
       {/* probably some errors but very important to keep in mind */}
       <Way1 />
     </>
