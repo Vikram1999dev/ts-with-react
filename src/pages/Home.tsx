5ea51fa9f4b5f3fb82dcc0ae13aa3856fa04534f
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2024-01-10 12:06:28 +0530

    explanation improvement

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,12 @@ export const Home = () => {
   return (
     <>
       {/* usage of assertion type */}
+      {/* the as keyword is primarily used for type assertions, which is
+       a way to tell the compiler to treat an expression as a different 
+       type than its inferred type.  */}
+      {/* it's important to understand that type assertions do not 
+       actually change the type of the variable at runtime. */}
+      {/* type assertion doesn't change the underlying type of the variable. */}
       <Way1 />
     </>
   );
